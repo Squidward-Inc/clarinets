ac87a9c27c45c5c0cd74317176eff7bf9cc8baa3
Brendan Law <lawbrendanm@gmail.com>
2023-06-22 16:29:05 -0400

    testing git stuff

--- src/dist/Components/Questions_Answers/QuestionsAnswers.jsx
+++ src/dist/Components/Questions_Answers/QuestionsAnswers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let QuestionsAnswers = ({product}) => {
-    return <h1>TESTING OUT THE Q&A EDITS</h1>
+    return <h1>STILL TESTING OUT THE Q&A EDITS</h1>
 };
 
 export default QuestionsAnswers;
